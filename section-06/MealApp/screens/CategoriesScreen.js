2c0a33a92ba2e0d8c09d7246769fb91136569249
hristo.s.hristov <hristo.s.hristov@mentormate.com>
2024-10-17 14:33:56 +0300

    creating meal app and install react navigation

---
+++ section-06/MealApp/screens/CategoriesScreen.js
@@ -0,0 +1,23 @@
+import { FlatList, StyleSheet } from "react-native";
+import { CATEGORIES } from "../data/dummy-data";
+import CategoryGridTile from "../components/CategoryGridTile";
+
+function CategoriesScreen() {
+  return (
+    <FlatList
+      data={CATEGORIES}
+      keyExtractor={(item) => item.id}
+      renderItem={(itemData) => (
+        <CategoryGridTile
+          title={itemData.item.title}
+          color={itemData.item.color}
+        />
+      )}
+      numColumns={2}
+    />
+  );
+}
+
+export default CategoriesScreen;
+
+const styles = StyleSheet.create({});
